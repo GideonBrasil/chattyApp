027b8f31c98977f5db68b71da0d1413a0204195a
GideonBrasil <gidxx_z@yahoo.com>
2019-03-12 14:29:55 +0000

    create initial state with loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,37 @@ import NavBar from "./NavBar.jsx";
 import Message from "./Message.jsx";
 import ChatBar from "./ChatBar.jsx";
 
+function Loading() {
+  return (
+    <nav className="navbar">
+      <a href="/" className="navbar-brand">
+        Chatty is Loading...
+      </a>
+    </nav>
+  );
+}
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 2000);
+  }
   render() {
-    return (
+    const asyncSection = this.state.loading ? (
+      <Loading />
+    ) : (
       <div>
         <NavBar />
         <Message />
         <ChatBar />
       </div>
     );
+    return <div>{asyncSection}</div>;
   }
 }
