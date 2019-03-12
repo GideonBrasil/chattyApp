692bd092277ab92e28357a58de5fefdc5d0b5000
GideonBrasil <gidxx_z@yahoo.com>
2019-03-12 00:58:12 +0000

    create ChatBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar.jsx";
 import Message from "./Message.jsx";
+import ChatBar from "./ChatBar.jsx";
 
 class App extends Component {
   render() {
-    return (
-    <NavBar />
-    <Message />
-    );
+    return <NavBar />, <Message />, <ChatBar />;
   }
 }
 export default App;
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
-<footer class="chatbar">
-  <input class="chatbar-username" placeholder="Your Name (Optional)" />
-  <input class="chatbar-message" placeholder="Type a message and hit ENTER" />
-</footer>;
+export default class ChatBar extends Component {
+  render() {
+    return (
+      <footer className="chatbar">
+        <input
+          className="chatbar-username"
+          placeholder="Your Name (Optional)"
+        />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+        />
+      </footer>
+    );
+  }
+}
