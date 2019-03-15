1568e34edc0cd4a53e429e07859d921fe5177cd5
GideonBrasil <gidxx_z@yahoo.com>
2019-03-15 00:33:13 +0000

    add style and icont to brand

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar({ clientCount }) {
   return (
     <nav className="navbar">
       <a href="/" className="navbar-brand">
-        Chatty
+        Chatty <i className="fab fa-rocketchat" />
       </a>
       <p className="clientCounter">
         {clientCount} user{clientCount > 1 ? "s" : ""} online
