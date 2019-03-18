643a915e4f93c77434fb7df9f480430717530afa
GideonBrasil <gidxx_z@yahoo.com>
2019-03-18 16:12:06 +0000

    fix color contrast

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -21,16 +21,8 @@ const wss = new SocketServer({
 
 // set up username color array
 let counter = 0;
-var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
-    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
-    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
-    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
-    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
+var colorArray = ['#333333', '#4a2e2e', '#5f1515', '#0d0202', '#1d4b59',
+    '#244852', '#2b2073', '#0a2d24', '#3e4210', '#4c1b3f'
 ];
 
 // Set up a callback that will run when a client connects to the server
@@ -39,7 +31,7 @@ var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
 wss.on('connection', (ws) => {
     // username color logic
     counter++
-    ws.uniqueColor = colorArray[counter % 50]
+    ws.uniqueColor = colorArray[counter % 10]
 
     // count connected users and send value to clients
     wss.clients.forEach(client => {
